67421d8aaa9f4c3fc3d88b0a2d88c9cc25dbe075
Ulrich Schmidt <u.schmidt@velian.de>
2017-02-14 15:07:07 +0100

    count

--- src/content/modules/chinese_and_russian_filter/hooks/before_init.php
+++ src/content/modules/chinese_and_russian_filter/hooks/before_init.php
@@ -1,5 +1,6 @@
 <?php
 if (ChiAndRuFilterHelper::checkPostRequests ()) {
+	Settings::set( "contact_form_refused_spam_mails", Settings::get( "contact_form_refused_spam_mails" ) + 1);
 	header ( "HTTP/1.0 403 Forbidden" );
 	die ( "Sorry! No chinese or russian allowed!" );
 }
